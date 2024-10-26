06c9167f521c8977aad81657d31965f75c1dad8c
thomas-illiet <contact@thomas-illiet.fr>
2024-10-26 23:17:59 +0200

    Fix grpc controller implementation

--- controller/grpc/activity.go
+++ controller/grpc/activity.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"github.com/rs/zerolog/log"
 	"github.com/terrapi-solution/controller/internal/core"
 	rpc "github.com/terrapi-solution/protocol/activity/v1"
 )
@@ -11,12 +12,16 @@ type GrpcActivityServer struct {
 	Services *core.Core
 }
 
-func (s *GrpcActivityServer) ListActivity(ctx context.Context, req *rpc.ListRequest) (*rpc.ListResponse, error) {
+func (s *GrpcActivityServer) List(ctx context.Context, req *rpc.ListRequest) (*rpc.ListResponse, error) {
 	activities := &rpc.ListResponse{}
 	return activities, nil
 }
 
-func (s *GrpcActivityServer) InsertActivity(ctx context.Context, req *rpc.InsertRequest) (*rpc.InsertResponse, error) {
+func (s *GrpcActivityServer) Insert(ctx context.Context, req *rpc.InsertRequest) (*rpc.InsertResponse, error) {
+
+	log.Info().Msg("InsertActivity")
+	log.Info().Msg(req.Message)
+
 	response := &rpc.InsertResponse{}
 	return response, nil
 }
