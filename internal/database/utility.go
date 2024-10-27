be2881e7cf88b402248a374f4bd8b6c29ea8473f
thomas-illiet <contact@thomas-illiet.fr>
2024-10-27 12:39:05 +0100

    Add utility to paginate database request

---
+++ internal/database/utility.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"gorm.io/gorm"
+)
+
+// Paginate applies pagination to a GORM query.
+func Paginate(page, pageSize int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		if page <= 0 {
+			page = 1
+		}
+
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset := (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
+}
