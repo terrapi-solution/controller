49d3a9f237e11919d32a980e5bec2fa82f1aa999
thomas-illiet <contact@thomas-illiet.fr>
2024-11-06 18:41:16 +0100

    Improve user model/store/service

--- data/user/schema.go
+++ data/user/schema.go
@@ -3,13 +3,14 @@ package user
 import "time"
 
 type User struct {
-	ID             int    `gorm:"primaryKey"`
-	Email          string `gorm:"not null;unique"`
-	HashedPassword string
-	FirstName      string
-	LastName       string
-	IsActive       bool
-	LastActive     time.Time
-	CreatedAt      time.Time `gorm:"autoCreateTime"`
-	UpdatedAt      time.Time `gorm:"autoUpdateTime"`
+	ID         int       `gorm:"primaryKey" filter:"filterable"`
+	Subject    string    `gorm:"not null;unique"`
+	UserName   string    `gorm:"not null;unique" filter:"searchable;filterable"`
+	FirstName  string    `filter:"filterable" filter:"searchable;filterable"`
+	LastName   string    `filter:"filterable" filter:"searchable;filterable"`
+	Email      string    `gorm:"not null" filter:"searchable;filterable"`
+	Status     bool      `filter:"filterable"`
+	LastActive time.Time `filter:"filterable"`
+	CreatedAt  time.Time `gorm:"autoCreateTime"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime"`
 }
