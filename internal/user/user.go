7b3e5f2b7c813462aa0a8b8c1fba1e064cd16ae8
thomas-illiet <contact@thomas-illiet.fr>
2024-11-15 14:47:25 +0100

    Remove user configuration because this product is technical api

--- internal/user/user.go
+++
@@ -1,8 +0,0 @@
-package user
-
-// User Represents information of current user
-type User struct {
-	Id       string `json:"id"`
-	UserName string `json:"username"`
-	Role     Role   `json:"role"`
-}
