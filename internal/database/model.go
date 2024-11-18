aa2cb33da4ab99f50267ee41b340fe9fee3f5114
thomas-illiet <contact@thomas-illiet.fr>
2024-11-18 13:41:28 +0100

    Add initial schema definitions for Plan and PlanVariable models.

--- internal/database/model.go
+++ internal/database/model.go
@@ -3,12 +3,16 @@ package database
 import (
 	"github.com/rs/zerolog/log"
 	"github.com/terrapi-solution/controller/data/module"
+	"github.com/terrapi-solution/controller/data/plan"
+	"github.com/terrapi-solution/controller/data/planVariable"
 )
 
 func CreateModel() {
 	// Define all database models
 	entities := []interface{}{
 		&module.Module{},
+		&plan.Plan{},
+		&planVariable.PlanVariable{},
 	}
 
 	// Execute auto migration for all models
